0978fe1a5d4d5896263424c0301a406450bcb761
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-09-13 13:24:16 +0200

    Add project files.

---
+++ Battle_IT3A/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Battle_IT3A
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private Unit player1;
+    private Unit player2;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      player1 = new Unit(500, 20);
+      player1.HitBox = new Rectangle();
+      player1.HitBox.Width = 40;
+      player1.HitBox.Height = 80;
+      player1.HitBox.Margin = new Thickness()
+      player2 = new Unit(500, 20);
+    }
+  }
+}
---
+++ Battle_IT3A/Unit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Battle_IT3A
+{
+  public class Unit
+  {
+    private int hp;   
+    public int HP
+    {
+      get => hp;
+      private set => Math.Max(Math.Min(value, 1000), 0);
+    }
+
+    public int DMG { get; internal set; } = 10;
+    public int Angle { get; set; } = 0;
+    public int Speed { get; set; } = 0;
+    public Rectangle HitBox { get; set; } = new Rectangle();
+
+    public Unit(int hp, int dmg)
+    {
+      HP = hp;
+      DMG = dmg;
+    }
+  }
+}
